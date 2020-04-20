397c28ec163216eeec16024df6921b49db77cfba
manita-tech <62539582+manita-tech@users.noreply.github.com>
2020-04-20 20:40:16 +0700

    Services on florist shop

--- Services.php
+++ Services.php
@@ -25,5 +25,99 @@
 <input type="text" placeholder="Search.." name="search"><button type="submit">Submit</button>
 </form></nav>';
 
-<h2> 
+<?php
+require('connect.php');
+$result = $conn->query("select * from Suppliers");
+?>
+
+<h2>Suppliers</h2>
+<table border="2" align="center">
+<tr>
+  <td><font color=gray><u>Shop_name</u></font></td>
+  <td><font color=gray><u>Supplier_Id </u></font></td>
+  <td><font color=gray><u>Address</u></font></td>
+  <td><font color=gray><u>Telephone__No</u></font></td>
+     <td><font color=gray><u>Email_Address</u></font></td>
+     <td><font color=gray><u>Bulk_Price</u></font></td>
+  
+</tr>
+
+  <?php
+var_dump($_POST);
+   ?> 
+
+<?php
+$sql = "SELECT * from Suppliers";
+$result = $conn->query($sql);
+$return_arr = array();
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo 
+"<tr>
+    <td>{$row['Shop_Name']}</td>
+    <td>{$row['Supplier_Id']}</td>
+    <td>{$row['Address']}</td>
+    <td>{$row['Telephone__No']}</td>
+        <td>{$row['Email_Address']}</td>
+    <td>{$row['Bulk_Price']}</td>
+  
+</tr>\n";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+    
+    <?php
+require('connect.php');
+$result = $conn->query("select * from Batches");
+?>
+
+<h2>Batches</h2>
+<table border="2" align="center">
+<tr>
+  <td><font color=gray><u>Delivered_time</u></font></td>
+  <td><font color=gray><u>Delivered_date</u></font></td>
+  <td><font color=gray><u>No_of_flowers </u></font></td>
+  <td><font color=gray><u> Batch__id</u></font></td>
+  <td><font color=gray><u>Type_of_flower</u></font></td>
+  <td><font color=gray><u>Supplier_Id</u></font></td>
+  
+</tr>
+
+  <?php
+var_dump($_POST);
+   ?> 
+
+<?php
+$sql = "SELECT * from Batches";
+$result = $conn->query($sql);
+$return_arr = array();
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo 
+"<tr>
+    <td>{$row['Delivered_time']}</td>
+    <td>{$row['Delivered_date']}</td>
+    <td>{$row['No_of_flowers']}</td>
+    <td>{$row[' Batch__id']}</td>
+    <td>{$row['Type_of_flower']}</td>
+    <td>{$row['Supplier_Id']}</td>
+  
+</tr>\n";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+    
+    
+
+    
+    
+
     
