231e08c7b2c5adf61f027080200ccb90e549b2c0
manita-tech <62539582+manita-tech@users.noreply.github.com>
2020-04-20 17:20:01 +0700

    Home page

---
+++ Home.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav nav class="navbar navbar-default">
+<div class="container-fluid">
+<div class="navbar-header">
+<a class="navbar-brand" href="#">Florist Shop</a>
+</div>
+<ul class="nav navbar-nav">
+<li class ="active"><a href="#">Home</a></li>
+<li ><a href="Flowers.php">Flowers</a></li>
+<li><a href="Categories.php">Categories</a></li>
+<li><a href="Services.php">Services</a></li>
+<li><a href="https://docs.google.com/document/d/1zCKMQn-ICuO0UYTWsbXoTXi_xjc6kwRg7ChzHJy5VZk/edit?usp=sharing"></i>Help</a></li>
+<li ><a href="About Us.php">About Us</a></li>
+</ul>
+<div class="search-container">
+<form action="/action_page.php">
+<input type="text" placeholder="Search.." name="search"><button type="submit">Submit</button>
+</form></nav>';
+    
+   <h1> Welcome to florist shop database🌼<h1>
