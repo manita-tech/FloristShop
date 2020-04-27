02a6d4c601f9e083ea0cd3561a4a9d2cc8fa6884
manita-tech <62539582+manita-tech@users.noreply.github.com>
2020-04-28 02:52:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,6 @@ body {
 <li class ="active"><a href="#">Home</a></li>
 <li ><a href="Flowers.php">Flowers</a></li>
 <li><a href="Categories.php">Categories</a></li>
-<li><a href="Services.php">Services</a></li>
 <li><a href="https://docs.google.com/document/d/1zCKMQn-ICuO0UYTWsbXoTXi_xjc6kwRg7ChzHJy5VZk/edit?usp=sharing"></i>Help</a></li>
 <li ><a href="About Us.php">About Us</a></li>
 </ul>
