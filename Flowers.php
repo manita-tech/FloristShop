bde8cb7decf5ccc3dfbccac16f0bc22004a80f22
manita-tech <62539582+manita-tech@users.noreply.github.com>
2020-04-27 07:21:28 +0700

    Update Flowers.php

--- Flowers.php
+++ Flowers.php
@@ -6,7 +6,12 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
-<body>
+ <body>
+<style>
+body {
+  background-color: mistyrose;
+  opacity: 0.8;
+}
 <nav nav class="navbar navbar-default">
 <div class="container-fluid">
 <div class="navbar-header">
@@ -64,8 +69,100 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
+ <?php
+require('connect.php');
+$result = $conn->query("select * from Customers");
+?>
 
-                                     
-</body>
+<h2>Customers</h2>
+<table border="2" align="center">
+<tr>
+  <td><font color=gray><u>Customer_Name</u></font></td>
+  <td><font color=gray><u>Flowers_Ordered</u></font></td>
+  <td><font color=gray><u>Selling_price</u></font></td>
+  <td><font color=gray><u>Customer_id</u></font></td>
+  
+</tr>
+
+  <?php
+var_dump($_POST);
+   ?> 
+
+<?php
+$sql = "SELECT * from Customers";
+$result = $conn->query($sql);
+$return_arr = array();
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo 
+"<tr>
+    <td>{$row['Customer_Name']}</td>
+    <td>{$row['Flowers_Ordered']}</td>
+    <td>{$row['Selling_price']}</td>
+    <td>{$row['Customer_id']}</td>
+  
+</tr>\n";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+   </script>
+<table id="t1">
+    <caption><font color="gray">Customer Table</font></caption>
+    <colgroup>
+        <col span="4" class="c4">
+        <col>
+        <col class="c4">
+    </colgroup>
+    <thead>
+        <tr>
+            <th>Customer Name</th>
+            <th>Flowers Ordered</th>
+            <th>Selling price</th>
+            <th> Customer id</th>
+        </tr>
+    </thead>
+    <tbody id="insertionPoint">
+    
+<h4><font color="gray">Add order</font></h4>
+<style>
+  table, td, th {border: 1px solid black; border-collapse: collapse;}
+</style>
+
+<div class="banner-info" align="center">
+    <span><label>Customer Name </label></span>
+    <span><input id="inputCustomerName" type="text" class="text-center" required="" placeholder="Enter Customer Name"></span><br>
+    <span><label>Flowers Ordered </label></span>
+    <span><input id="inputFlowersOrdered" type="text" class="text-center" required=""></span><br>
+    <span><label>Selling price </label></span>
+    <span><input id="inputSellingprice" type="text" class="text-center" required=""></span><br>
+     <span><label>Customer id </label></span>
+     <span><input id="inputCustomerid" type="text" class="text-center" required=""></span><br>
+</div>
+<div class="logo">
+    <input type="button" onClick="insertData()" value="Add entry">
+</div>
+<script language="javascript">
+    function insertData() {
+   var name = document.getElementById("inputCustomerName").value;
+    var orders =document.getElementById("inputFlowersOrdered").value;
+   var price = document.getElementById("inputSellingprice").value;
+   var id = document.getElementById("inputCustomerid").value;
+        document.getElementById("insertionPoint").innerHTML += "<tr><td>" + name + "</td><td>" + orders + "</td><td>" + price "</td><td>" + id + "</td></tr>";
+      
+        // The below part is to clear the values after the entry is added.
+        document.getElementById("inputCustomerName").value = "";
+        document.getElementById("inputFlowersOrdered").value = "";
+        document.getElementById("inputSellingprice").value = "";
+        document.getElementById("inputCustomerid").value = "";
+    }
+    }
+      
+    </tbody>
+</table
+</div>
 
         
