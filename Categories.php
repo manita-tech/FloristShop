3d19dbca66ce08cab6f2462195738b42b962843b
manita-tech <62539582+manita-tech@users.noreply.github.com>
2020-04-28 06:17:51 +0700

    Update Categories.php

--- Categories.php
+++ Categories.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html lang="en">
 <html>
 <head>
 <meta charset="utf-8">
@@ -6,21 +8,22 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
-<<style>
+
+<style>
 body {
-  background-color: mistyrose;
-  opacity: 0.8;
+  background-color: lavenderblush;
+  opacity: 0.7;
 }
+</style>
 <nav nav class="navbar navbar-default">
 <div class="container-fluid">
 <div class="navbar-header">
 <a class="navbar-brand" href="#">Florist Shop</a>
 </div>
 <ul class="nav navbar-nav">
-<li ><a href="Home.php">Home</a></li>
+<li ><a href="#">Home</a></li>
 <li ><a href="Flowers.php">Flowers</a></li>
 <li class ="active"><a href="Categories.php">Categories</a></li>
-<li><a href="Services.php">Services</a></li>
 <li><a href="https://docs.google.com/document/d/1zCKMQn-ICuO0UYTWsbXoTXi_xjc6kwRg7ChzHJy5VZk/edit?usp=sharing"></i>Help</a></li>
 <li ><a href="About Us.php">About Us</a></li>
 </ul>
@@ -29,76 +32,21 @@ body {
 <input type="text" placeholder="Search.." name="search"><button type="submit">Submit</button>
 </form></nav>
     
-<h3> Currently flower sells: </h3>
-<?php
-require('connect.php');
-$result = $conn->query("select * from Sells");
-?>
-
-<table border="2" align="center">
-<tr>
-  <td><font color=gray><u>Flower_id</u></font></td>
-  <td><font color=gray><u>SSN</u></font></td>
-  
-</tr>
-
-<?php
-$sql = "SELECT * from Sells";
-$result = $conn->query($sql);
-$return_arr = array();
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo 
-"<tr>
-    <td>{$row['Flower_id']}</td>
-    <td>{$row['SSN']}</td>
-  
-</tr>\n";
-    }
-} else {
-    echo "0 results";
+    
+    <style>
+  h3.italic {
+  font-style: italic;
 }
-$conn->close();
-?>
-?php
-require('connect.php');
-$result = $conn->query("select * from Sells");
-?>
+</style>
+<center><h3 style="color:grey" class="italic">Categories of seasonal flowers</h3></center>
 
-<table border="2" align="center">
-<tr>
-  <td><font color=gray><u>Flower_id</u></font></td>
-  <td><font color=gray><u>Batch__id</u></font></td>
-  
-</tr>
+<center> <img src= "https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/2399895/910/607/m1/fpnw/wm0/1-.jpg?1489396646&s=f340289662b6d978404cae9a53b63286" width="500" height="300"></center>
 
-<h3> Currently flowers have:</h3>
 
-<?php
-$sql = "SELECT * from Have";
-$result = $conn->query($sql);
-$return_arr = array();
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo 
-"<tr>
-    <td>{$row['Flower_id']}</td>
-    <td>{$row['Batch__id']}</td>
-  
-</tr>\n";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
-?>
-?>
-?php
-require('connect.php');
-$result = $conn->query("select * from Have");
-?>
-                                  
-</body>
+<center> <img src= "https://previews.123rf.com/images/foxyliam/foxyliam1703/foxyliam170300957/73691876-summer-flowers-set-vector-illustration.jpg" width="500" height="300"></center>
+
+
+<center> <img src= "https://previews.123rf.com/images/olgakorneeva/olgakorneeva1911/olgakorneeva191100007/133951593-vintage-vector-botanical-illustration-set-autumn-flowers-colorful.jpg" width="500" height="300"></center>
+<center> <img src= "https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v783-noon-69_1.jpg?auto=format&bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&mark=rawpixel-watermark.png&markalpha=90&markpad=13&markscale=10&markx=25&q=75&usm=15&vib=3&w=800&s=851411ce2f9ada1122a3376e284e94e1" width="500" height="300"></center>
+
 
