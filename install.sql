7c7975d71dd6ca7df11fddd3c4f3464c6cd4ebb4
manita-tech <62539582+manita-tech@users.noreply.github.com>
2020-04-28 02:26:54 +0700

    Update install.sql

--- install.sql
+++ install.sql
@@ -41,16 +41,12 @@ CREATE TABLE have
   FOREIGN KEY (Batch__id) REFERENCES Batches(Batch__id)
 );
 
-INSERT INTO Batches (Delivered_time,Delivered_date,No_of_flowers,Batch__id) VALUES ("2020-07-11 18:44:23","2020-07-11 16:53:48",324,34),("2020-07-18 01:09:42","2020-07-18 16:41:19",567,45),("2020-07-22 03:16:54","2020-07-22 06:15:49",800,22);
+INSERT INTO Batches (Delivered_time,Delivered_date,No_of_flowers,Batch__id) VALUES ("2020-07-11 18:44:23","2020-07-11 16:53:48","324","34"),("2020-07-18 01:09:42","2020-07-18 16:41:19","567","45"),("2020-07-22 03:16:54","2020-07-22 06:15:49","800","22");
 
 INSERT INTO Customers (Customer_Name,Flowers_Ordered,Customer_id) VALUES ("Talon","244","14"),("Bianca","459","27"),("Aphrodie","361","80") ;
 
-INSERT INTO Flower_for_sale (Type_of_flower,Flower_id,Price) VALUES ("White Lotus",20,"73.97"),("Red Roses",49,"87.48"),(" Yellow Daffodils ",17,"63.66");
+INSERT INTO Flower_for_sale (Type_of_flower,Flower_id,Price) VALUES ("White Lotus","20","73.97"),("Red Roses","49","87.48"),(" Yellow Daffodils ","17","63.66");
 
-INSERT INTO Selects (Flower_id,Customer_id) VALUES (20,14),(49,27),(17,80);
+INSERT INTO Selects (Flower_id,Customer_id) VALUES ("20","14"),("49","27"),("17","80");
 
-INSERT INTO have (Flower_id,Batch__id) VALUES (20,34),(49,45),(17,22),
-
-
-
-
+INSERT INTO have (Flower_id,Batch__id) VALUES ("20","34"),("49","45"),("17","22");
